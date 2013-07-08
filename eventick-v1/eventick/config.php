18e8431dd190d0666762ae5c38a5bde6f2725caf
Sébastien Robaszkiewicz <sebastien.robaszkiewicz@gmail.com>
2013-07-08 21:21:51 +0200

    update DB in EvenTick v1

--- eventick-v1/eventick/config.php
+++ eventick-v1/eventick/config.php
@@ -3,9 +3,9 @@
 /* Database Connection */
 
 $sDbHost = 'localhost';
-$sDbName = 'kjanus_test';
-$sDbUser = 'kjanus_admin';
-$sDbPwd = 'ghdg786';
+$sDbName = 'eventick_v1';
+$sDbUser = 'root';
+$sDbPwd = '';
 
 $dbConn = mysql_connect ($sDbHost, $sDbUser, $sDbPwd) or die ('MySQL connect failed. ' . mysql_error());
 mysql_select_db($sDbName,$dbConn) or die('Cannot select database. ' . mysql_error());
