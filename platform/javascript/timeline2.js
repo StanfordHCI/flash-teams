07cebbae1ed041b88184b4196f01559e224b3145
Alexandra To <ato1120@stanford.edu>
2013-07-12 08:13:29 -0700

    Added d3 gantt code

--- platform/javascript/timeline2.js
+++ platform/javascript/timeline2.js
@@ -15,6 +15,26 @@ timeline_svg.append("rect")
     .attr("fill", "white")
     .on("mousedown", mousedown); 
 
+//Taken from d3.gantt By Dimitry Kudrayvtsev
+//START WORKING HERE -AT
+var timeDomainStart = d3.time.day.offset(new Date(),-3);
+var timeDomainEnd = d3.time.hour.offset(new Date(),+3);
+var timeDomainMode = "fixed"; 
+var tickFormat = "%H:%M";
+var xAxis = d3.svg.axis().scale(x).orient("bottom").tickFormat(d3.time.format(tickFormat)).tickSubdivide(true)
+        .tickSize(8).tickPadding(8);
+var yAxis = d3.svg.axis().scale(y).orient("left").tickSize(0);
+var x = d3.time.scale().domain([ timeDomainStart, timeDomainEnd ]).range([ 0, width ]).clamp(true);
+var y = d3.scale.ordinal().domain(taskTypes).rangeRoundBands([ 0, height - margin.top - margin.bottom ], .1);
+var timeDomain = 10; //ARBITRARY LENGTH FOR INITIAL TIMELINE, CHOOSE BETTER TIME
+var initAxis = function() {
+    x = d3.time.scale().domain([ timeDomainStart, timeDomainEnd ]).range([ 0, width ]).clamp(true);
+    y = d3.scale.ordinal().domain(taskTypes).rangeRoundBands([ 0, height - margin.top - margin.bottom ], .1);
+    xAxis = d3.svg.axis().scale(x).orient("bottom").tickFormat(d3.time.format(tickFormat)).tickSubdivide(true)
+        .tickSize(8).tickPadding(8);
+    yAxis = d3.svg.axis().scale(y).orient("left").tickSize(0);
+    };
+
 
 var task_rectangles = [],
     task_rectangle = timeline_svg.selectAll(".task_rectangle");
@@ -45,7 +65,7 @@ function restart() {
         .attr("fill-opacity", .5)
         .attr('pointer-events', 'all')
         .on('click', function() { //BREAKS AUTHORING OF RECTANGLES
-            $('a.timeline_info').popover({
+            $(this).popover('toggle'){
             title: 'New Event',
             placement: 'right',
             content: '<button id="delete">Delete</button>'
