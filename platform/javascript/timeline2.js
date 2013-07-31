2a7b7fbcd790a116f2f89a60b4c0c7a77882d043
Alexandra To <ato1120@stanford.edu>
2013-07-31 11:03:44 -0700

    Snapping to row

--- platform/javascript/timeline2.js
+++ platform/javascript/timeline2.js
@@ -1,5 +1,6 @@
-/*
- *
+/* Timeline2.js
+ * ----------------------
+ * 
  *
  */
 
@@ -24,11 +25,13 @@ var timeline_svg = d3.select("#timeline-container").append("svg")
     .attr("height", height)
     .attr("class", "chart");
 
+var XTicks = 14,
+    YTicks = 10;
+
 //CHART CODE (http://synthesis.sbecker.net/articles/2012/07/11/learning-d3-part-4-intro-to-svg-graphics)
-//START WORKING HERE -AT
 //Draw x grid lines
 timeline_svg.selectAll("line.x")
-    .data(x.ticks(10)) //CHOOSE TICKS LATER
+    .data(x.ticks(XTicks))
     .enter().append("line")
     .attr("class", "x")
     .attr("x1", x)
@@ -39,7 +42,7 @@ timeline_svg.selectAll("line.x")
 
 //Draw y axis grid lines
 timeline_svg.selectAll("line.y")
-    .data(y.ticks(10)) //CHOOSE TICKS LATER
+    .data(y.ticks(YTicks)) 
     .enter().append("line")
     .attr("class", "y")
     .attr("x1", 0)
@@ -48,12 +51,12 @@ timeline_svg.selectAll("line.y")
     .attr("y2", y)
     .style("stroke", "#5F5A5A");
 
-//Add X Axis Labels
+//Add X Axis Labels, FIX THIS
 timeline_svg.selectAll(".rule")
-    .data(x.ticks(10)) //ADJUST LATER
+    .data(x.ticks(XTicks)) 
     .enter().append("text")
     .attr("x", x)
-    .attr("y", 0)
+    .attr("y", 20)
     .attr("dy", -3)
     .attr("text-anchor", "middle")
     .text(String);
@@ -85,10 +88,13 @@ var task_rectangles = [],
 
 function mousedown() {
     event_counter++; //To generate id
-    var point = d3.mouse(this),
-        task_rectangle = {x: point[0], y: point[1], id: event_counter};
-    task_rectangles.push(task_rectangle);
+    var point = d3.mouse(this);
+    var snapX = Math.floor(point[0]),
+        snapY = Math.floor(point[1]/rectangle_height) * rectangle_height;
 
+    var task_rectangle = {x: snapX, y: snapY, id: event_counter};
+
+    task_rectangles.push(task_rectangle);
     restart();
 }
 
@@ -112,14 +118,10 @@ function restart() {
             return rectId; }) 
         .attr("height", rectangle_height)
         .attr("width", rectangle_width)
-        .attr("fill", "red")
+        .attr("fill", "#C9C9C9")
         .attr("fill-opacity", .6)
         .attr("stroke", "#5F5A5A")
-        .attr('pointer-events', 'all') 
-        .on('click', function(d) { 
-            $("a[rel=eventpopover]").toggle(); //toggles visibility of elements, PROBLEM: SELECTS ALL EVENT POPOVERS
-            console.log("You clicked rect" + d.id); //DEBUGGING
-        });
+        .attr('pointer-events', 'all');
     
     task_rectangle = timeline_svg.selectAll(".task_rectangle").data(task_rectangles, function (d) { return d.id});
     task_rectangle.exit().remove(); 
@@ -142,54 +144,23 @@ function restart() {
                 container: $("#timeline-container")
             });
         });
-
-    //Event Popover
-    /*$("#timeline-container").css("position","relative");
-    $("#timeline-container").append($("<a>", 
-    {
-        href: "#",
-        class: "event",
-        id: event_counter, 
-        rel: "eventpopover", 
-        position:"absolute",
-        "data-toggle": "popover",
-        title: '<form name="eventHeaderForm_' + event_counter + '"><input type = "text" name="eventtitle"></form>',
-        html: "+", //TOGGLES ON THIS
-    }).css("position","absolute").css("left",dx+90).css("top",dy+100)); //Ethan Fast help
-
-    $("a[rel=eventpopover]").popover({
-        placement: "bottom",
-        html: "true",
-        content: '<form name="eventForm_' + event_counter + '"><h10>Total Runtime: <input type = "text" name = "totalruntime"></h10>' 
-            +'Happening From: <input type = "time" name="starttime"><br>' + ' To: <input type = "time" name="endtime>'
-            +'Members<input type = "textfield" name="members"><br>'
-            +'<p><button type="button" id="delete" onclick="deleteRect(' + event_counter +');">Delete</button>  ' 
-            +'<button type="button" id="done" onclick="hidePopover(' + event_counter + ');">Done</button> </p>' 
-            +'</form>'
-    });*/
 };
 
 function hidePopover(popId) {
     $("#rect" + popId).popover("hide");
-    console.log("You hid popover" + popId);
-}
+};
 
 function deleteRect(rectId) {
     $("#rect" + rectId).popover("destroy");
-    
     var element = null;
-    var index = 0;
     for (var i = 0; i < task_rectangles.length; i++) {
         element = task_rectangles[i];
-        console.log("rectId", element.id, "index", i);
         if (element.id == rectId) {
             task_rectangles.splice(i, 1);
             restart();
-            console.log("Deleted rect" + rectId + ", task_rectangles length is now " + task_rectangles.length);
             break;
         }
     }
-    
 };
 
 
