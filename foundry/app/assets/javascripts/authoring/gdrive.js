247b8c20d67001bcda02903f4b5b930eefdb74ca
Jay Patel <jayhp9@stanford.edu>
2014-03-30 20:51:01 -0700

    runtime timeline all fixed

--- foundry/app/assets/javascripts/authoring/gdrive.js
+++ foundry/app/assets/javascripts/authoring/gdrive.js
@@ -13,7 +13,7 @@ overallFolder = ["0B6l5YPiF_QFBUUNvNWxyZXJaRGM", "https://docs.google.com/a/stan
 function handleClientLoad() {
   // gapi.load("auth:client,drive-realtime,drive-share", callback);
   checkAuth();
-}
+};
 
 /**
  * Check if the current user has authorized the application.
@@ -22,7 +22,7 @@ function checkAuth() {
   gapi.auth.authorize(
       {'client_id': CLIENT_ID, 'scope': SCOPES.join(' '), 'immediate': true},
       handleAuthResult2);
-}
+};
 
 /**
  * Called when authorization server replies.
@@ -41,19 +41,19 @@ function handleAuthResult2(authResult) {
         {'client_id': CLIENT_ID, 'scope': SCOPES, 'immediate': false},
         handleAuthResult2);
   }
-}
+};
 
 /* File-picker javascript files*/
 function onAuthApiLoad() {
   gapi.auth.authorize(
     {'client_id': CLIENT_ID, 'scope': SCOPES, 'immediate': true},
     handleAuthResult);
-}
+};
 
 function onApiLoad(){
       gapi.load('auth', {'callback': onAuthApiLoad});
       gapi.load('picker');
-}
+};
 
 var oauthToken;
 function handleAuthResult(authResult){
@@ -61,7 +61,7 @@ function handleAuthResult(authResult){
     oauthToken = authResult.access_token;
     createPicker();
   }
-}
+};
 
 function createPicker(){
   var docUpload = new google.picker.DocsUploadView();
@@ -72,23 +72,22 @@ function createPicker(){
     .setCallback(pickerCallback)
     .build()
   picker.setVisible(true);
-}
+};
 
 function pickerCallback(data){
   if (data.action == google.picker.Action.PICKED){
     alert('URL: ' + data.docs[0].url);
   }
-}
-
+};
 
 function createNewFolder(eventName, JSONId){
-   console.log(eventName);
-   console.log(folderIds);
-   gapi.client.load('drive', 'v2', function() {
-
-   if (flashTeamsJSON.folder){
-       console.log("This one", flashTeamsJSON.folder);
-       var request = gapi.client.request({
+  //console.log(eventName);
+  //console.log(folderIds);
+   
+  gapi.client.load('drive', 'v2', function() {
+    var req;
+    if (flashTeamsJSON.folder){
+      req = gapi.client.request({
         'path': '/drive/v2/files',
         'method': 'POST',
         'body':{
@@ -98,10 +97,9 @@ function createNewFolder(eventName, JSONId){
             "parents": [{"id": flashTeamsJSON.folder[0]}]
          }
       });
-    }
-     else{
-      console.log("Nope, this one");
-      var request = gapi.client.request({
+    } else {
+      //console.log("Nope, this one");
+      req = gapi.client.request({
         'path': '/drive/v2/files',
         'method': 'POST',
         'body':{
@@ -110,34 +108,26 @@ function createNewFolder(eventName, JSONId){
             "description" : "Overall Shared Folder"
          }
       });
-     }
+    }
 
-      resp = request.execute(function(resp) { 
-        var folderArray = [resp.id, resp.alternateLink];
-        if (!flashTeamsJSON.folder){
-          console.log("overall ", folderArray);
-          insertPermission(folderArray[0], "me", "anyone", "writer");
-          console.log("preFolder", flashTeamsJSON.folder);
-          flashTeamsJSON.folder = folderArray;
-          console.log("postFolder", flashTeamsJSON.folder);
-        }
-        else{
-          flashTeamsJSON["events"][JSONId].gdrive = folderArray;
-          console.log(folderIds);
-          console.log("adding gdrive");
-          console.log(flashTeamsJSON["events"][JSONId]);
-          folderIds.push(folderArray);
-        }
-        return resp; 
-      });
-   });
-}
+    req.execute(function(resp) { 
+      var folderArray = [resp.id, resp.alternateLink];
+      if (!flashTeamsJSON.folder) {
+        insertPermission(folderArray[0], "me", "anyone", "writer");
+        flashTeamsJSON.folder = folderArray;
+      } else {
+        flashTeamsJSON["events"][JSONId].gdrive = folderArray;
+        folderIds.push(folderArray);
+      }
+    });
+  });
+};
 
 function addAllFolders(){
   for (var i = 0; i<flashTeamsJSON["events"].length; i++){
     createNewFolder(flashTeamsJSON["events"][i].title + flashTeamsJSON["events"][i].id, i);
   }
-}
+};
 
 function createNewFile(eventName) {
 
@@ -156,7 +146,7 @@ function createNewFile(eventName) {
 
       request.execute(function(resp) { console.log(resp); });
    });
-}
+};
 
 function deleteFile(fileId){
   gapi.client.load('drive', 'v2', function(){
@@ -165,21 +155,18 @@ function deleteFile(fileId){
     });
     request.execute(function(resp) { });
   });
-}
-
+};
 
 function insertPermission(fileId, value, type, role) {
-  gapi.client.load('drive', 'v2', function() {
-    var body = {
-      'value': value,
-      'type': type,
-      'role': role,
-      'withLink': true
-    };
-    var request = gapi.client.drive.permissions.insert({
-      'fileId': fileId,
-      'resource': body
-    });
-    request.execute(function(resp) { console.log(resp)});
+  var body = {
+    'value': value,
+    'type': type,
+    'role': role,
+    'withLink': true
+  };
+  var request = gapi.client.drive.permissions.insert({
+    'fileId': fileId,
+    'resource': body
   });
-}
+  request.execute(function(resp) { });
+};
