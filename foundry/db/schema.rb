40ba6316a44b7e1694eb5fa8beb1274292ea3a13
Alexandra To <ato1120@stanford.edu>
2014-07-29 10:52:48 -0700

    database update

--- foundry/db/schema.rb
+++ foundry/db/schema.rb
@@ -57,16 +57,6 @@ ActiveRecord::Schema.define(version: 20140729170357) do
     t.datetime "updated_at"
   end
 
-  create_table "sessions", force: true do |t|
-    t.string   "session_id", null: false
-    t.text     "data"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "sessions", ["session_id"], name: "index_sessions_on_session_id", unique: true, using: :btree
-  add_index "sessions", ["updated_at"], name: "index_sessions_on_updated_at", using: :btree
-
   create_table "skills", force: true do |t|
     t.string   "name"
     t.datetime "created_at"
@@ -87,12 +77,6 @@ ActiveRecord::Schema.define(version: 20140729170357) do
     t.integer  "end_minute"
     t.integer  "duration"
     t.text     "description"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "users", force: true do |t|
-    t.string   "username"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
