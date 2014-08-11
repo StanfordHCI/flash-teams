b51ec591361f593fcabe4586321fb70251e2dfc8
dretelny <dretelny@stanford.edu>
2014-08-11 15:02:16 -0700

    removed commented out code

--- foundry/app/assets/javascripts/authoring/sidebar.js
+++ foundry/app/assets/javascripts/authoring/sidebar.js
@@ -124,28 +124,7 @@ $('#messageInput').keydown(function(e){
     }
 });
 
-//load all chats that were sent before page was reloaded
-/*myDataRef.once('value', function(snapshot) {
-    var message = snapshot.val();
-    
-	if(message != null){
-		 displayChatMessage(message.name, message.uniq, message.role, message.date, message.text);
-    
-		 name = message.name;
-	}
-});*/
 
-/*myDataRef.on('child_added', function(snapshot) {
-    var message = snapshot.val();
-    //console.log(snapshot);
-    //console.log(message);
-    //console.log("MESSAGE NAME: " + message["name"]);
-
-    displayChatMessage(message.name, message.uniq, message.role, message.date, message.text);
-    
-    name = message.name;
-});
-*/
 var lastMessage=0;
 var lastWriter;
 
